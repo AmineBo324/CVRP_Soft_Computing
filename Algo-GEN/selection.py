cc565b25f0fedb1acf33e9542eb07582161517fb
maryem38 <hajamormaryem38@gmail.com>
2026-01-17 23:00:33 +0100

    Update selection.py: adding: determinist, rank, remainder, and uniformr selection methode

--- Algo-GEN/selection.py
+++ Algo-GEN/selection.py
@@ -5,6 +5,7 @@ def tournament_selection(population, fitnesses, k=3):
     return max(selected, key=lambda x: x[1])[0]
 
 
+
 def roulette_wheel_selection(population, fitnesses):
     total = sum(fitnesses)
     pick = random.uniform(0, total)
